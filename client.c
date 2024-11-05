bb062217f3fe5c1f3271a94c99667e01d527ba7a
smminassian <sebastienminassian203@gmail.com>
2024-11-04 22:57:06 -0800

    Updated client.c

--- client.c
+++ client.c
@@ -11,11 +11,19 @@
 
 int main(int argc, char* argv[]){
     if(argc == 2){
-        char message[20] = argv[1];
-        int -p = atoi(argv[2]);
+        //Taking command line arguments
+        char message[20];
+        strncopy(message, argv[1], sizeof(message) - 1)
+        int port = atoi(argv[2]);
+
+
         struct sockaddr_in serv_addr;
         int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
 
+        if(socket_fd < 0){
+            perror("Socket creation error");
+            return -1;
+        }
         
     }
 }
